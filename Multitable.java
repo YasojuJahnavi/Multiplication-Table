853954a22c1d5338818fae3fd761791a77558396
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 12:10:54 +0530

    Update Multitable.java

--- Multitable.java
+++ Multitable.java
@@ -5,7 +5,7 @@ public class Multitable {
         System.out.println("enter a number");
         int num =scanner.nextInt();
         for(int i=1;i<=10;i++){
-            System.err.println(num+"*"+i+"="+num*i);
+            System.err.println(num+" * "+i+" = "+num*i);
         }
         scanner.close();
     }
